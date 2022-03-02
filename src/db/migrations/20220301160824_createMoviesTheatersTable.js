f7edf17bdd6c4674135ffb4ba467554560852fbb
johmert <johmert@users.noreply.github.com>
2022-03-01 16:21:33 -0800

    "is_showing" added to moviestheaters

--- src/db/migrations/20220301160824_createMoviesTheatersTable.js
+++ src/db/migrations/20220301160824_createMoviesTheatersTable.js
@@ -12,6 +12,7 @@ exports.up = function(knex) {
             .references("theater_id")
             .inTable("theaters")
             .onDelete("CASCADE");
+        table.boolean("is_showing");
         table.timestamps(true, true);
     });  
 };
