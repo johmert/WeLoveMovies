1c7447f92a454e532fdd413c208e6f4f619e5f0f
johmert <johmert@users.noreply.github.com>
2022-03-11 15:13:12 -0800

    adding cors package

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 if (process.env.USER) require("dotenv").config();
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
+const cors = require("cors");
 
 const express = require("express");
 
@@ -9,7 +10,7 @@ const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router"); 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
